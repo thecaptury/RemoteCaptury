56d2d8ec1f8dbbd429184a5adf5353b7173bcfab
Rohit Kumar <rkumar@darimotion.com>
2022-11-10 17:31:36 +0530

    added a python script for test.

---
+++ testcallback.py
@@ -0,0 +1,31 @@
+# add location of remotecaptury.pyd or remotecaptury.so
+path = "./build/Release/"
+import sys
+sys.path.append(path)
+
+
+import remotecaptury as rc
+import time
+
+print(f"start-")
+def callback(args):
+	print("callback - python")
+	print(f"type of image = {type(args)}")
+	print(args)
+
+# start streaming images.
+print(f"trying to connect")
+connected = rc.connect(host = '127.0.0.1')
+print(f"is_connected = {connected}")
+
+streaming_start = rc.startStreamingImages(0, 0)
+print(f"is_streaming = {streaming_start}")
+
+if (streaming_start):
+	# set new callback function.
+	callback_registered = rc.setNewImageCallback(callback)
+	print(f"is_callback_registered = {callback_registered}")
+
+	# wait for 10 seconds.
+	print(f"waiting for 10 seconds to receive images")
+	time.sleep(10)
