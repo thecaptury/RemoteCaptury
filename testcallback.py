92a9be7422e7100d7379859ef729b5f2ed3aba08
Rohit Kumar <rkumar@darimotion.com>
2022-11-15 11:56:14 +0530

    saving the images for test.

--- testcallback.py
+++ testcallback.py
@@ -8,22 +8,29 @@ sys.path.append(path)
 
 import remotecaptury as rc
 import time
+import numpy as np
+from PIL import Image
+import shutil
+if os.path.exists("./logs"):
+	shutil.rmtree("./logs")
+os.mkdir("./logs")
 
-log_path = os.path.abspath("./testcallback.log")
-log_path2 = os.path.abspath("./testcallback2.log")
+log_path = os.path.abspath("./logs/testcallback.log")
+log_path2 = os.path.abspath("./logs/testcallback2.log")
 count = 0
 
-def callback():
-	global count
-	count += 1
-	with open(log_path, "a") as f:
-		f.write(f"callback {count}\n")
 
-def callback2():
+def callback(*args):
 	global count
 	count += 1
-	with open(log_path2, "a") as f:
-		f.write(f"callback2 {count}\n")
+	image, width, height = args
+	image = image.reshape(height, width, 3)
+	print(f"min max of image : {np.min(image)} {np.max(image)}, dtype : {image.dtype}")
+	if isinstance(image, np.ndarray):
+		print(image.shape)
+		# convert to PIL image
+		image = Image.fromarray(image)
+		image.save(f"./logs/testcallback_{count}.png")
 
 # start streaming images.
 print(f"trying to connect")
@@ -37,10 +44,6 @@ print(f"is_callback_registered = {callback_registered}")
 streaming_start = rc.startStreamingImages(cameraNumber=0)
 print(f"is_streaming = {streaming_start}")
 
-time.sleep(3)
-
-callback_registered = rc.setNewImageCallback(callback2)
-print(f"is_callback2_registered = {callback_registered}")
 time.sleep(3)
 
 streaming_stop = rc.stopStreaming()
